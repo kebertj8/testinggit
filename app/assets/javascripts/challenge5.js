ee9a48d6708e52c8d4cbe657d2f1f3c01e8640b1
kebertj8 <Kebertj8@gmail.com>
2019-12-06 22:12:26 -0500

    WIP

---
+++ app/assets/javascripts/challenge5.js
@@ -0,0 +1,32 @@
+// function plusMinus(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (i === i) {
+//       (i++)/arr.length
+//     }else{
+//       (i)/arr.length 
+//     }
+//   }
+// }
+
+function plusMinus(arr) {
+  let positives = 0, negatives = 0, zeros = 0;
+  const len = arr.length || 0;
+
+  if (len > 0 && len <= 100) {
+    arr.map((elem, key) => {
+      if (elem > 0) {
+        positives++;
+      } else if (elem < 0) {
+        negatives++;
+      } else {
+        zeros++;
+      }
+
+      return elem;
+    });
+  }
+
+  console.log((positives / len) || 0);
+  console.log((negatives / len) || 0);
+  console.log((zeros / len) || 0);
+}
