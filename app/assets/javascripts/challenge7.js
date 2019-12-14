004dd196b1d62e3fcf5b9cb32a2a93c88a9413e5
kebertj8 <Kebertj8@gmail.com>
2019-12-13 19:37:12 -0500

    Min-max sum WIP learning to manipulate indices

---
+++ app/assets/javascripts/challenge7.js
@@ -0,0 +1,19 @@
+function miniMaxSum(arr) {
+  let arr = []
+  let arr2 = arr - arr.min
+  for (let i = 0; i < arr.length; i++) {
+    arr += arr.reduce[i]
+    arr2 += arr2.reduce[i]
+    
+  }
+  return Math.abs(arr)
+  return Math.abs(arr2)
+}
+
+
+
+// define the variables
+// 2 arrays
+// 1 that takes away the first or smallest indices
+// count each
+// var array using.max etc
