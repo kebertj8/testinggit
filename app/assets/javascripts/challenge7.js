720e3ae975a6005e3198e804636db77b75235f87
kebertj8 <Kebertj8@gmail.com>
2019-12-14 18:40:40 -0500

    WIP

--- app/assets/javascripts/challenge7.js
+++ app/assets/javascripts/challenge7.js
@@ -1,19 +1,21 @@
-function miniMaxSum(arr) {
-  let arr = []
-  let arr2 = arr - arr.min
-  for (let i = 0; i < arr.length; i++) {
-    arr += arr.reduce[i]
-    arr2 += arr2.reduce[i]
+// function miniMaxSum(arr) {
+//   let arr = []
+//   let arr2 = arr - arr.min
+//   for (let i = 0; i < arr.length; i++) {
+//     arr += arr.reduce[i]
+//     arr2 += arr2.reduce[i]
     
-  }
-  return Math.abs(arr)
-  return Math.abs(arr2)
+//   }
+//   return Math.abs(arr)
+//   return Math.abs(arr2)
+// }
+
+function miniMaxSum(arr) {
+  let sortedArray = arr.sort()
+  let minArray = sortedArray.slice(0, -1)
+  let maxArray = sortedArray.slice(1)
+  let add = (a, b) => a + b
+  let maxSum = maxArray.reduce(add)
+  let minSum = minArray.reduce(add)
+  console.log(minSum + " " + maxSum)
 }
-
-
-
-// define the variables
-// 2 arrays
-// 1 that takes away the first or smallest indices
-// count each
-// var array using.max etc
