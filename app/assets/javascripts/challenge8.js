02dc20d2634c49c31f7a0e1490a634481aa48dcf
kebertj8 <Kebertj8@gmail.com>
2019-12-20 01:12:49 -0500

    WIP Birthday Candles Hackerrank

---
+++ app/assets/javascripts/challenge8.js
@@ -0,0 +1,30 @@
+// function birthdayCakeCandles(ar) {
+//   let maxCandleHeight = Integer.Min_Value
+//   let maxCandleFreqCount = 0;
+
+//   for (let i = 0; i < ar.length; i++){
+//     if (ar[i] == maxCandleHeight){
+//       maxCandleFreqCount++;
+//     }
+//     if (ar[i] > maxCandleHeight) {
+//       maxCandleHeight = ar[i];
+//       maxCandleFreqCount = 1;
+//     }
+//   }
+//   return maxCandleFreqCount; 
+// }
+
+// function birthdayCakeCandles(ar) {
+//   let tallest = Math.max(...ar)
+//   return ar.filter((candleHeight) => tallest === candleHeight).length
+// }
+
+function birthdayCakeCandles(ar) {
+  let tallest = []
+  let disregard = 0;
+  for (let i = 0; i < ar.length; i++){
+      if([i] == disregard){
+        
+      }
+  }
+}
