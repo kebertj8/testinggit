b3f90def380222bc3c1413f85ba6369a3f00c1da
kebertj8 <Kebertj8@gmail.com>
2019-12-24 23:43:30 -0500

    should work

--- app/assets/javascripts/challenge7.js
+++ app/assets/javascripts/challenge7.js
@@ -1,15 +1,3 @@
-// function miniMaxSum(arr) {
-//   let arr = []
-//   let arr2 = arr - arr.min
-//   for (let i = 0; i < arr.length; i++) {
-//     arr += arr.reduce[i]
-//     arr2 += arr2.reduce[i]
-    
-//   }
-//   return Math.abs(arr)
-//   return Math.abs(arr2)
-// }
-
 function miniMaxSum(arr) {
   let sortedArray = arr.sort()
   let minArray = sortedArray.slice(0, -1)
