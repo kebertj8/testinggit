5961530730142fc2d6454f7e5d86444b49625485
kebertj8 <Kebertj8@gmail.com>
2020-02-07 20:03:32 -0500

    12-24hr time conversion-regex exploration

--- app/assets/javascripts/challenge9.js
+++ app/assets/javascripts/challenge9.js
@@ -1,20 +1,31 @@
-// 1/17/20
-function timeConversion(s){
-  var time = readLine();
-  var s = 12;
-  var hour = time.replace("PM", ":*").replace("AM", ":/").split(":");
-  if (hour[3] == '/') {
-    if (hour[0] == "12") {
-      hour[0] = '00';
-    }
-  } else {
-    if (hour[0] == "12") {
-      hour[0] = '12';
-    } else {
-      var s = s + parseInt(hour[0]);
-      s.toString();
-      hour[0] = s;
-    }
-  }
-  console.log(hour[0] + ':' + hour[1] + ':' + hour[2])
-}
+// // 1/17/20
+// function timeConversion(s){
+//   var time = readLine();
+//   var s = 12;
+//   var hour = time.replace("PM", ":*").replace("AM", ":/").split(":");
+//   if (hour[3] == '/') {
+//     if (hour[0] == "12") {
+//       hour[0] = '00';
+//     }
+//   } else {
+//     if (hour[0] == "12") {
+//       hour[0] = '12';
+//     } else {
+//       var s = s + parseInt(hour[0]);
+//       s.toString();
+//       hour[0] = s;
+//     }
+//   }
+//   console.log(hour[0] + ':' + hour[1] + ':' + hour[2])
+// }
+
+// 2/7/20
+// function timeConversion(s) {
+//   var time = readLine();
+//   var s = 12;
+//   if (time === /(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/) {
+//     return time
+//   } else {
+//     console.log("test")
+//   }
+// }
