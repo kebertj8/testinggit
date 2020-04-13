97bcd6684cc8cf923c498202993eeb953c706add
kebertj8 <Kebertj8@gmail.com>
2020-04-13 17:19:38 -0400

    Grade function

--- app/assets/javascripts/grades.js
+++ app/assets/javascripts/grades.js
@@ -1,40 +1,14 @@
 function gradingStudents(grades) {
   let finalGrade = []
-  for (let i = 0; i <= grades.length; i++) {
-    if (grades[i] <= 40) {
+
+  for (let i = 0; i < grades.length; i++) {
+    if (grades[i] < 38 || grades[i] % 5 < 3) {
       finalGrade.push(grades[i])
     }
-    if (grades[i] >= 40) {
+    else {
       finalGrade.push(Math.ceil(grades[i] / 5) * 5)
     }
   }
+
   return finalGrade
 }
-
-
-
-function gradingStudents(grades){
-
-  let finalGrade = []
-  let roundedGrade = []
-  
-  for(let i = 0; i <= grades.length; i++){
-    if(grades[i] < 40 && grades[i] < 38 ){
-      finalGrade.push(grades[i])
-    }
-    else if (grades[i] >= 38) {
-      roundedGrade.push(Math.ceil(grades[i]/5) * 5)
-    }
-    for (let y = 0; y <= roundedGrade.length; y++){
-      let difference = roundedGrade[i] - grades[i]
-      if (difference < 3){
-        finalGrade.push(roundedGrade[i])
-      } 
-      else if (difference === 3){
-        finalGrade.push(grades[i])
-      }
-    }
-  }
-  
-  return finalGrade
-  }
