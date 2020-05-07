a5e1b6a0d55cf3979c40060eb2adbc6a617c2405
kebertj8 <Kebertj8@gmail.com>
2020-05-06 21:36:31 -0400

    Latex Code Conversion in progress

---
+++ app/assets/javascripts/nonEuclideanGeometry.js
@@ -0,0 +1,28 @@
+// Saving Files to convert later
+
+
+// \documentclass{ article }
+// \title{ Visualization Of Non - Euclidean Geometry }
+// \date{ April 21st 2020 }
+// \author{ Kebert Joseph }
+// \begin{ document }
+// \maketitle
+// \section{ Introduction }
+// Euclidean Geometry is based an earlier knowledge of geometry dating back 300BC.Defined by the five Euclid(Named after Mathematician Alexandrian Euclid)Axioms or Common Notions.
+// \newline
+// 1. A straight line may be drawn from any given point to any other
+// \newline
+// 2. A straight line may be extended to any finite length
+// \newline
+// 3. A circle may be described with any given point as its center and any distance as its radius.
+// \newline
+// 4. All right angles are congruent
+// \newline
+// 5. If a straight line intersects two other straight lines, and so makes the two interior angles on one side of it together less than the two right angles, then the other straight lines will meet at a point if extended far enough on the side on which the angles are less than two right angles.
+// \newline
+// \newline
+// Formally defined as:
+// $d$, ($P$, $Q$) = $\sqrt { $(p_{ 1}$ - $q_{ 1}$) ^ 2 - $(p_{ 2}$ - $q_{ 2})$ ^ 2 } $
+// \newline
+// \newline
+// \end{ document }
