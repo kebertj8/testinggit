84fc51c052a50324a8c3c1f75bb41a07caf8d477
kebertj8 <Kebertj8@gmail.com>
2020-05-24 13:07:23 -0400

    latex

--- app/assets/javascripts/nonEuclideanGeometry.js
+++ app/assets/javascripts/nonEuclideanGeometry.js
@@ -1,13 +1,12 @@
-// Saving Files to convert later
-
-
 // \documentclass{ article }
+// \usepackage{ graphicx }
+// \graphicspath{ {./images/ } }
 // \title{ Visualization Of Non - Euclidean Geometry }
 // \date{ April 21st 2020 }
 // \author{ Kebert Joseph }
 // \begin{ document }
 // \maketitle
-// \section{ Introduction }
+// \section * { Euclidean Geometry }
 // Euclidean Geometry is based an earlier knowledge of geometry dating back 300BC.Defined by the five Euclid(Named after Mathematician Alexandrian Euclid)Axioms or Common Notions.
 // \newline
 // 1. A straight line may be drawn from any given point to any other
@@ -21,8 +20,51 @@
 // 5. If a straight line intersects two other straight lines, and so makes the two interior angles on one side of it together less than the two right angles, then the other straight lines will meet at a point if extended far enough on the side on which the angles are less than two right angles.
 // \newline
 // \newline
-// Formally defined as:
-// $d$, ($P$, $Q$) = $\sqrt { $(p_{ 1}$ - $q_{ 1}$) ^ 2 - $(p_{ 2}$ - $q_{ 2})$ ^ 2 } $
+// The distance between two points is formally defined as:
+// \[
+//   d, (P, Q) = \sqrt{(p_1 - q_1) ^ 2 + (p_2 - q_2) ^ 2}
+//   \]
 // \newline
 // \newline
+// \subsection * { Non Euclidean Geometry }
+// Non - euclidean geometry arises when either the metric requirement is relaxed, or when the fifth axiom as noted above, also known as the parallel postulate is replaced with an alternative one.In the latter one obtains hyperbolic and elliptic geometry.The traditional Non - Euclidean Geometry.
+
+// \includegraphics{ lines.png }
+// \newline
+// \newline
+// \paragraph{ Line }
+// A line can be formally described as a straight one - dimensional figure having no thickness and extending infinitely in both directions.
+// \newline
+// A line segment is a part of a line that is bounded by two distinct end points, and contains every point on the line between its endpoints.
+// \newline
+// In $R ^ 2$, every line $L$(including vertical lines) is described by a linear equation of the form
+// \[
+//   L = {(x, y) | ax + by = c}
+//   \]
+// with real coefficients $a, b$ and $c$ such that $a$ and $b$ are both not zero.Using this form, vertical lines correspond to the equation with $b = 0$
+// \newline
+// $R ^ 2$ is the distance between two points coming from the Pythagorean Theorem.
+// Points in $R ^ n$ are represented as $x= (x_1,...x_n), $ where $x_1,..., x_n, $ are real numbers, and adding subscripts to a point in $R ^ n$ will always represent its coordinates.
+
+// \paragraph{ The hyperbolic plane } $H ^ 2$ is the open upper half plane in $R ^ 2$, and $hyperbolic lines$ are the vertical half lines and semi - circles orthogonal to the $x - axis$.
+// \newline
+// \newline
+// \includegraphics{ hyperbolic.png }
+// \subsection * { Visualization of triangles }
+// \paragraph{ Hyperbolic Geometry }
+// In hyperbolic geometry the sum of the angles of a hyperbolic triangle is strictly less than $\pi$ radians $(180 ^\circ)$, a straight angle.The difference is referred to as the defect.
+// In contrast to Euclidean Geometry where as the angles always add up to $\pi$ radians $180 ^\circ$
+// \paragraph{ Elliptic geometry } can be visualized as the surface of a sphere on which "lines" are taken as great circles.In elliptic geometry, the sum of angles of a triangle is $ > $ $180 ^\circ$
+// \paragraph{ Spherical Geometry } is the geometry of the two dimensional surface of a sphere.
+// Two most notable applications of the principles of spherical geometry are navigation and astronomy.
+// \newline
+// \includegraphics{ globe.png }
+// \newline
+// \textbf{ Sphere } on a sphere the sum of the angles of a triangle is not equal to $180 ^\circ$.A sphere is a non - euclidean space, but locally the laws of the Euclidean Geometry are good approximations.In a small triangle on the face of the earth, (as seen on the photo above) the sum of the angles is $ > $ $180 ^\circ$
+// \newline
+// \includegraphics{ hyperbolic - triangle.png }
+// \newline
+// Hyperbolic triangle embedded in a saddle - shaped surface.
+// \subsection * { Understanding Applications }
+
 // \end{ document }
