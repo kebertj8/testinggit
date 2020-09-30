89a1bb565780c38ca75e42e361761a8a8544f2b2
kebertj8 <Kebertj8@gmail.com>
2020-09-29 23:11:15 -0400

    WIP

---
+++ app/assets/javascripts/inverselatex.js
@@ -0,0 +1,199 @@
+\documentclass{ article }
+\usepackage[utf8]{ inputenc }
+\usepackage{ graphicx }
+\graphicspath{ {./images/ } }
+
+\title{ Problem Set #3 }
+\author{ Kebert Joseph }
+\date{ September 27 2020 }
+
+\begin{ document }
+
+\maketitle
+
+\includegraphics[width = 160mm, scale = 0.75]{ 1.png }
+
+A.$h(x) = (2x + 1)^ 2$
+\[
+  f(x) = x ^ 2
+  \]
+\[
+  g(x) = 2x + 1
+  \]
+
+B.$h(x) = $ $ ^ 4\sqrt{ 3x ^ 2 + 4 } +2$
+
+\[
+  f(x) = ^ 4\sqrt{ x } + 2
+  \]
+\[
+  g(x) = 3x ^ 2 + 4
+  \]
+
+C.$h(x) = \frac{ 5 } { (3x - 1)^ 4 } $
+\[
+  f(x) = \frac{ 5}{ x^ 4}
+  \]
+\[
+  g(x) = 3x - 1
+  \]
+
+D.$h(x) = \frac{ -(x - 2) ^ 2 + 3 } { 5 } $
+\[
+  f(x) = \frac{- x ^ 2 + 3}{ 5}
+  \]
+\[
+  g(x) = x - 2
+  \]
+
+\includegraphics[width = 160mm, scale = 0.75]{ 2.png }
+
+\newline
+$o$ = Oldest
+\newline
+$m$ = middle
+\newline
+$y$ = youngest
+\newline
+
+A.
+\[
+  o(m) = 2m + 3
+  \]
+\[
+  m(y) = \frac{ x }{ 3} + 8
+  \]
+\[
+  x
+  \]
+
+
+B.
+\[
+  o(m) = 23 = 2x + 3
+  \]
+\[
+  o(m) = 23 - 3 = 2x
+  \]
+\[
+  o(m) = \frac{ 20}{ 2x }
+  \]
+\[
+  m = 10
+  \]
+
+\[
+  m(y) = 10 = \frac{ y }{ 3} + 8
+  \]
+\[
+  m(y) = 10 - 8 = \frac{ y }{ 3}
+  \]
+\[
+  y = 6
+  \]
+
+\includegraphics[width = 160mm, scale = 0.75]{ 3.png }
+a.
+  $f(x) = x ^ 6$
+\newline
+Doesn't pass the horizontal line test 
+
+b.
+  $f(x) = 3x ^ 2$
+\[
+  f(x) = 3x + 2
+  \]
+\[
+  x = 3f ^ -1(x) + 2
+  \]
+\[
+  f ^ -1(x) = \frac{ x- 2}{ 3}
+  \]
+\newline
+
+c.
+  $f(x) = \frac{ 1 } { x - 3 } $
+\[
+  f(x) = \frac{ 1}{ x- 3}
+  \]
+\[
+  x = \frac{ 1}{ f^ -1(x) - 3}
+  \]
+\[
+  x = (\frac{ 1}{ f^ -1(x) - 3})* f ^ -1(x) - 3
+  \]
+\[
+  f ^ -1(x) - 3(x) = 1 + 3
+  \]
+\[
+  f ^ -1(x) = \frac{ 1}{ x } + 3
+  \]
+
+\newline
+
+d.
+  $f(x) = x ^ 2 + 3x + 2$
+\newline
+Doesn't pass the horizontal line test 
+
+\newline
+
+e.
+  $f(x) = \frac{ 2x - 1 } { 5x + 2 } $
+\[
+  x = \frac{ 2f ^ -1(x) - 1 }{ 5f ^ -1(x) + 2 }
+  \]
+
+\[
+  x = (\frac{ 2f ^ -1(x) - 1 }{ 5f ^ -1(x) + 2 })* 5f ^ -1(x) + 2
+  \]
+\[
+  (5f ^ -1(x) + 2) (x) = (\frac{ 2f ^ -1(x) - 1 }{ x })* (x)
+  \]
+\[
+  (5f ^ -1(x) + 2) (x) = { 2f ^ -1(x) - 1 }
+  \]
+\[
+  5f ^ -1(x)(x) + 2x = { 2f ^ -1(x) - 1 }
+  \]
+\[
+  5f ^ -1(x)(x) + 2x - { 2f ^ -1(x) + 1 } = 0
+  \]
+\[
+  f ^ -1(x)(5x - 2) = -2x - 1
+  \]
+\[
+  f ^ -1(x) = \frac{- 2x - 1}{ 5x - 2 }
+  \]
+
+\newline
+
+f.
+  $f(x) = \frac{ 6x - 10 } { 7x + 15 } $
+\[
+  x = \frac{ 6f ^ -1(x) - 10 }{ 7f ^ -1(x) + 15 }
+  \]
+\[
+  x = (\frac{ 6f ^ -1(x) - 10 }{ 7f ^ -1(x) + 15 })* 7f ^ -1(x) + 15
+  \]
+\[
+  7f ^ -1(x) + 15(x) = (\frac{ 6f ^ -1(x) - 10 }{ x })* x
+  \]
+\[
+  7f ^ -1(x) + 15(x) - 6f ^ -1(x) + 10 = 0
+  \]
+\[
+  7f ^ -1(x) + 15(x) - 6f ^ -1(x) + 10 = -15x - 10
+  \]
+\[
+  f ^ -1(x)(7x - 6) = -15x - 10
+  \]
+\[
+  f ^ -1(x) = \frac{- 15x - 10}{ 7x - 6 }
+  \]
+
+\includegraphics[width = 160mm, scale = 0.75]{ 4.png }
+
+\includegraphics[width = 160mm, scale = 0.75]{ 5.png }
+
+\end{ document }
